4257f34baef89d0c1e90748b38c6325c60023e52
adamzwickey <azwickey@vmware.com>
2020-10-13 17:45:27 -0400

    adding auto-build

---
+++ cluster1.sh
@@ -0,0 +1,5 @@
+kubectx aws-workload1-admin@aws-workload1
+kubectl apply -f todos-redis/app.yml
+kubectl apply -f todos-api/app.yml
+kubectl apply -f todos-edge/app.yml
+kubectl apply -f todos-webui/app.yml
---
+++ cluster2.sh
@@ -0,0 +1,2 @@
+kubectx aws-workload2-admin@aws-workload2
+kubectl apply -f todos-postgres/app.yml
