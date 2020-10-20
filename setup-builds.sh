9e599fd5fd2179c90911f9617f909db236f014f7
adamzwickey <azwickey@vmware.com>
2020-10-20 13:51:41 -0400

    changed harbor registry URL

--- setup-builds.sh
+++ setup-builds.sh
@@ -1,14 +1,14 @@
-kp image create todos-api --tag harbor.mgmt.tanzu.zwickey.net/library/todos-api \
+kp image create todos-api --tag registry.mgmt.tanzu.zwickey.net/library/todos-api \
  --git https://gitlab.com/azwickey/todos.git --env BP_MAVEN_BUILT_MODULE=todos-api
 
-kp image create todos-edge --tag harbor.mgmt.tanzu.zwickey.net/library/todos-edge \
+kp image create todos-edge --tag registry.mgmt.tanzu.zwickey.net/library/todos-edge \
  --git https://gitlab.com/azwickey/todos.git --env BP_MAVEN_BUILT_MODULE=todos-edge
 
-kp image create todos-postgres --tag harbor.mgmt.tanzu.zwickey.net/library/todos-postgres \
+kp image create todos-postgres --tag registry.mgmt.tanzu.zwickey.net/library/todos-postgres \
  --git https://gitlab.com/azwickey/todos.git --env BP_MAVEN_BUILT_MODULE=todos-postgres
 
-kp image create todos-redis --tag harbor.mgmt.tanzu.zwickey.net/library/todos-redis \
+kp image create todos-redis --tag registry.mgmt.tanzu.zwickey.net/library/todos-redis \
  --git https://gitlab.com/azwickey/todos.git --env BP_MAVEN_BUILT_MODULE=todos-redis
 
-kp image create todos-webui --tag harbor.mgmt.tanzu.zwickey.net/library/todos-webui \
+kp image create todos-webui --tag registry.mgmt.tanzu.zwickey.net/library/todos-webui \
  --git https://gitlab.com/azwickey/todos.git --env BP_MAVEN_BUILT_MODULE=todos-webui
