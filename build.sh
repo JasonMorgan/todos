fa8067bc787f5cbb8efe135c70712560244c82bd
adamzwickey <azwickey@vmware.com>
2020-11-07 14:11:41 -0500

    added argo apps

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
+export HARBOR=registry.private.tanzu.zwickey.net/library
+
 mvn clean install -DskipTests
 docker build -t azwickey/todos-edge todos-edge/
-docker tag azwickey/todos-edge harbor.mgmt.tanzu.zwickey.net/library/todos-edge:latest
-docker push harbor.mgmt.tanzu.zwickey.net/library/todos-edge:latest
+docker tag azwickey/todos-edge $HARBOR/todos-edge:latest
+docker push $HARBOR/todos-edge:latest
 
 docker build -t azwickey/todos-webui todos-webui/
-docker tag azwickey/todos-webui harbor.mgmt.tanzu.zwickey.net/library/todos-webui:latest
-docker push harbor.mgmt.tanzu.zwickey.net/library/todos-webui:latest
+docker tag azwickey/todos-webui $HARBOR/todos-webui:latest
+docker push $HARBOR/todos-webui:latest
 
 docker build -t azwickey/todos-api todos-api/
-docker tag azwickey/todos-api harbor.mgmt.tanzu.zwickey.net/library/todos-api:latest
-docker push harbor.mgmt.tanzu.zwickey.net/library/todos-api:latest
+docker tag azwickey/todos-api $HARBOR/todos-api:latest
+docker push $HARBOR/todos-api:latest
 
 docker build -t azwickey/todos-redis todos-redis/
-docker tag azwickey/todos-redis harbor.mgmt.tanzu.zwickey.net/library/todos-redis:latest
-docker push harbor.mgmt.tanzu.zwickey.net/library/todos-redis:latest
+docker tag azwickey/todos-redis $HARBOR/todos-redis:latest
+docker push $HARBOR/todos-redis:latest
 
 docker build -t azwickey/todos-postgres todos-postgres/
-docker tag azwickey/todos-postgres harbor.mgmt.tanzu.zwickey.net/library/todos-postgres:latest
-docker push harbor.mgmt.tanzu.zwickey.net/library/todos-postgres:latest
+docker tag azwickey/todos-postgres $HARBOR/todos-postgres:latest
+docker push $HARBOR/todos-postgres:latest
